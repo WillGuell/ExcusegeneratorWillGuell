24e2b42feec426ba31a80362ccdd387f134fbe99
WillGuell <william.guell001@gmail.com>
2025-06-23 21:07:04 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+ function generateExcuse() {
+  const who = ['My dog', 'The neighbor', 'A bird', 'My grandma', 'The mailman'];
+  const action = ['ate', 'broke', 'hid', 'destroyed', 'took'];
+  const what = ['my homework', 'my phone', 'the car keys', 'my laptop', 'the project'];
+  const when = ['this morning', 'last night', 'just now', 'while I was sleeping', 'before class'];
 
+  const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${random(who)} ${random(action)} ${random(what)} ${random(when)}.`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById('excuse').innerHTML = generateExcuse();
 };
